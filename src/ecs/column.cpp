7421d9a58bd267d5b39952f9dfe37065120cd004
undersilence <metarufolds@gmail.com>
2022-12-21 04:38:38 +0800

    Update generic columns implementation.

---
+++ src/ecs/column.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 立 on 2022/12/21.
+//
+
+#include "column.hpp"
---
+++ src/test/test_column.cpp
@@ -0,0 +1,13 @@
+//
+// Created by 立 on 2022/12/21.
+//
+
+#include "ecs/column.hpp"
+
+class Dummy {
+  int x;
+};
+
+int main() {
+  return 0;
+}
